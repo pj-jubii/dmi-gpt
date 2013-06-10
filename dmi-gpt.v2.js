10e69f18365c2143597332ffcf14c9934283d847
soreng <soreng@gmail.com>
2013-06-10 16:04:29 +0300

    Update and rename dmi-gpt.v1.js to dmi-gpt.v2.js

--- dmi-gpt.v2.js
+++ dmi-gpt.v2.js
@@ -72,7 +72,7 @@ function gptFindPageUnit(path) {
 				for (x in u.r) 
 				{
 					if(typeof(ms[x]) === 'undefined') continue;
-					kv[u.r[x]] = ms[x];
+					kv[kv.length] = { 'k':u.r[x]], 'v':ms[x]};
 				};
 			}
 
@@ -85,7 +85,7 @@ function gptFindPageUnit(path) {
 		
 googletag.cmd.push(function () {
 	var pageUnit = gptFindPageUnit(document.location.pathname);
-	for (var i = 0; i < pageUnit.kv.length; i++) {var kv = pageUnit.kv[i];googletag.pubads().setTargeting(kv.key, kv.value);}
+	for (var i = 0; i < pageUnit.kv.length; i++) {var kv = pageUnit.kv[i];googletag.pubads().setTargeting(kv.k, kv.v);}
 	googletag.defineSlot('/7733/' + pageUnit.au + '/930x180_atf_top1', [930, 180], 'gpt-930x180-atf-top1').addService(googletag.pubads()).setTargeting('fold','atf').setTargeting('position','top1');
 	googletag.defineSlot('/7733/' + pageUnit.au + '/160x600_atf_right1', [160, 600], 'gpt-160x600-atf-right1').addService(googletag.pubads()).setTargeting('fold','atf').setTargeting('position','right1');
 	googletag.defineOutOfPageSlot('/7733/' + pageUnit.au + '/1x1_outofpage1', 'gpt-1x1-oop').addService(googletag.pubads()).setTargeting('position', 'outofpage1');
